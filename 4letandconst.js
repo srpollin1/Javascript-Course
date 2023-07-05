d18fa33d7d01cc004958c02385c0c275f2272f4f
Eduardo Retamales <80782448+srpollin1@users.noreply.github.com>
2023-07-05 09:56:54 -0400

    Actualizar 4letandconst.js

--- 4letandconst.js
+++ 4letandconst.js
@@ -1,22 +1,22 @@
-// let name
-// name = "eduardo"
-// console.log(name)
-// name = "Edward"
-// console.log(name)
+let name
+name = "eduardo"
+console.log(name)
+name = "Edward"
+console.log(name)
  
-// const name = "Pedro"
-// name = "Cristian"
+const name = "Pedro"
+name = "Cristian"
 
-// const user = {
-//     name: 'Alan',
-//     age: 24
-// }
-// console.log(user)
-// user.name = "Jhon"
-// console.log(user)
+const user = {
+    name: 'Alan',
+    age: 24
+}
+console.log(user)
+user.name = "Jhon"
+console.log(user)
 
-// const arr = []
-// console.log(arr)
-// arr.push(3)
-// console.log(arr)
+const arr = []
+console.log(arr)
+arr.push(3)
+console.log(arr)
 
