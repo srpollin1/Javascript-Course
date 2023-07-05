dead9e544508f8e9d5e2b81c38747a1389a63154
Eduardo Retamales <80782448+srpollin1@users.noreply.github.com>
2023-07-05 09:55:53 -0400

    Actualizar 3variables.js

--- 3variables.js
+++ 3variables.js
@@ -1,17 +1,17 @@
-// var name = 'Eduardo Retamales'
-// console.log(name)
-// name = 'Edward Snowden'
-// console.log(name)
+var name = 'Eduardo Retamales'
+console.log(name)
+name = 'Edward Snowden'
+console.log(name)
 
-// var message
-// console.log(message)
-// message = "New message"
-// console.log(message)
+var message
+console.log(message)
+message = "New message"
+console.log(message)
 
 // letters, number, _ , $
 // X start with numbers
-// var $name = 'Some name'
+var $name = 'Some name'
 
-// var firstName //CamelCase
-// var firt_name //UnderScore
-// var FirtName //PascalCase
+var firstName //CamelCase
+var firt_name //UnderScore
+var FirtName //PascalCase
