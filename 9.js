38c8774cee7bb7adfe81410bbe30848b328b22a4
Eduardo Retamales <80782448+srpollin1@users.noreply.github.com>
2023-07-06 11:39:37 -0400

    update number and math models

--- 9.js
+++ 9.js
@@ -1,36 +1,36 @@
-// const num1 = 8
-// const num2 = 6
-// let val
-// // Addition
-// val = num1 + num2
-// 
-// // Subtraction
-// val = num1 - num2
-// 
-// // Multiplication
-// val = num1 * num2
-// 
-// // Divion
-// val = num1 / num2
-// 
-// // Modulus (Remainder)
-// val = num1 % num2
-// 
-// // Math Object
-// val = Math.PI
-// val = Math.round(8.7)
-// val = Math.round(8.4)
-// val = Math.pow(8.2)
-// val = Math.sqrt(8.4)
-// val = Math.abs(-4)
-// val = Math.ceil(8.1)
-// val = Math.floor(8.4)
-// val = Math.min(2, 6, 1)
-// val = Math.max(2, 6, 1)
-// 
-// //Math.random() returns a random
-// //number between 0 (inclusive), and 1 (exclusive):
-// val = Math.random() 
-// //Random between 1 to 5
-// val = Math.floor(Math.random() * 5 + 1) 
-// console.log(val)
+const num1 = 8
+const num2 = 6
+let val
+// Addition
+val = num1 + num2
+
+// Subtraction
+val = num1 - num2
+
+// Multiplication
+val = num1 * num2
+
+// Divion
+val = num1 / num2
+
+// Modulus (Remainder)
+val = num1 % num2
+
+Math Object
+val = Math.PI
+val = Math.round(8.7)
+val = Math.round(8.4)
+val = Math.pow(8.2)
+val = Math.sqrt(8.4)
+val = Math.abs(-4)
+val = Math.ceil(8.1)
+val = Math.floor(8.4)
+val = Math.min(2, 6, 1)
+val = Math.max(2, 6, 1)
+
+//Math.random() returns a random
+//number between 0 (inclusive), and 1 (exclusive):
+val = Math.random() 
+//Random between 1 to 5
+val = Math.floor(Math.random() * 5 + 1) 
+console.log(val)
