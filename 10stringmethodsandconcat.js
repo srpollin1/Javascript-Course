d81e090956b56f5431a26df120a7487cf79c8c26
Eduardo Retamales <80782448+srpollin1@users.noreply.github.com>
2023-07-06 12:00:22 -0400

    update file 10 string and methodsandconcat

--- 10stringmethodsandconcat.js
+++ 10stringmethodsandconcat.js
@@ -11,3 +11,18 @@ val = firstName + ' ' + lastName
 val = 'Cristian'
 val += 'Rojas'
 
+//Escaping
+val = "That's great. You've found a job"
+val = 'That\'s great. You\'ve found a job'
+
+//Length
+val = firstName.length
+
+//Concat
+val = firstName.concat('',lastName)
+
+//Change case
+val = firstName.toUpperCase()
+val = firstName.toLowerCase()
+
+console.log(val)
